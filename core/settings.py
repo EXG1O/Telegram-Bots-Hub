84a2bfac72fc6da6fbbe7b6016bf365f62427aae
exg1o <borodenko22448811@gmail.com>
2025-01-18 20:07:39 +0200

    Add option for connect to service API via unix sockets

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from yarl import URL
 
+from pathlib import Path
 from secrets import token_hex
 from typing import Final
 import os
@@ -15,4 +16,7 @@ SELF_TOKEN: Final[str] = os.environ['SELF_TOKEN']
 TELEGRAM_TOKEN: Final[str] = token_hex(32)
 
 SERVICE_URL: Final[URL] = URL(os.environ['SERVICE_URL'])
+SERVICE_UNIX_SOCK: Final[Path | None] = (
+	Path(path) if (path := os.getenv('SERVICE_UNIX_SOCK')) else None
+)
 SERVICE_TOKEN: Final[str] = os.environ['SERVICE_TOKEN']
