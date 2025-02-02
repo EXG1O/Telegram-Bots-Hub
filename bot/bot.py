62372722713dd8ac816af6f9e473a629da1f788c
exg1o <borodenko22448811@gmail.com>
2025-02-02 13:52:37 +0200

    Update bot utils

--- bot/bot.py
+++ bot/bot.py
@@ -31,7 +31,7 @@ import service.base_models
 import service.enums
 import service.models
 
-from .utils import process_text_with_html, replace_text_variables
+from .utils import html, replace_text_variables
 
 from datetime import UTC, datetime, timedelta
 from typing import Any
@@ -93,7 +93,7 @@ class Bot:
 
         variables.update(
             {
-                variable.name: await process_text_with_html(variable.value)
+                variable.name: await html.process_text(variable.value)
                 for variable in await self.service_api.get_variables()
             }
         )
@@ -278,7 +278,7 @@ class Bot:
 
         kwargs: dict[str, Any] = {'chat_id': chat_id}
         message_text: str = await replace_text_variables(
-            await process_text_with_html(command.message.text), variables
+            await html.process_text(command.message.text), variables
         )
         keyboard: (
             ReplyKeyboardMarkup | InlineKeyboardMarkup | None
