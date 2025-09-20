d9e0964a247e78bf11459eb7fe5b52d5c8cba5f3
exg1o <77574092+EXG1O@users.noreply.github.com>
2025-09-20 23:19:34 +0300

    Rename `text` bot util to `variables` and update it (#22)

--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,5 +1,10 @@
 from .html import process_html_text
-from .text import replace_text_variables
 from .validation import is_valid_user
+from .variables import replace_data_variables, replace_text_variables
 
-__all__ = ['process_html_text', 'replace_text_variables', 'is_valid_user']
+__all__ = [
+    'process_html_text',
+    'replace_text_variables',
+    'replace_data_variables',
+    'is_valid_user',
+]
