18c2e7d9c2840a020467a360d9ae3f2cfa18e5d7
exg1o <exg1o.me@gmail.com>
2025-09-20 23:13:19 +0300

    Rename `text` bot util to `variables` and update it

--- bot/utils/variables.py
+++ bot/utils/variables.py
@@ -31,3 +31,25 @@ async def replace_text_variables(text: str, variables: Variables) -> str:
     result.append(text[last_end_index:])
 
     return ''.join(result)
+
+
+async def replace_data_variables(data: Any, variables: Variables) -> Any:
+    if isinstance(data, str):
+        return await replace_text_variables(data, variables)
+    elif isinstance(data, tuple | list | set | frozenset):
+        return type(data)(
+            await asyncio.gather(
+                *[replace_data_variables(item, variables) for item in data]
+            )
+        )
+    elif isinstance(data, dict):
+        keys, values = await asyncio.gather(
+            asyncio.gather(
+                *[replace_data_variables(key, variables) for key in data.keys()]
+            ),
+            asyncio.gather(
+                *[replace_data_variables(value, variables) for value in data.values()]
+            ),
+        )
+        return dict(zip(keys, values, strict=False))
+    return data
