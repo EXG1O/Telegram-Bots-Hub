fde38ab7b02f89d1fc48348acd0f41514e286884
exg1o <exg1o.me@gmail.com>
2026-02-24 22:46:28 +0200

    Add `style` field to `MessageKeyboardButton` model in `service` package

--- service/api.py
+++ service/api.py
@@ -14,6 +14,7 @@ from .enums import (
     ConditionPartType,
     ConnectionSourceObjectType,
     ConnectionTargetObjectType,
+    MessageKeyboardButtonStyle,
     MessageKeyboardType,
 )
 from .models import (
@@ -47,6 +48,7 @@ config = Config(
             ConnectionTargetObjectType,
             APIRequestMethod,
             MessageKeyboardType,
+            MessageKeyboardButtonStyle,
             ConditionPartType,
             ConditionPartOperator,
             ConditionPartNextPartOperator,
