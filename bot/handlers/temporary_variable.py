236c487f74ede4e715e5febcee77717f3aa47343
exg1o <77574092+EXG1O@users.noreply.github.com>
2026-03-19 03:35:17 +0200

    Use `TEMPORARY` namespace in `TemporaryVariableHandler` bot handler (#60)

--- bot/handlers/temporary_variable.py
+++ bot/handlers/temporary_variable.py
@@ -6,12 +6,15 @@ from ..context import HandlerContext
 from ..utils.variables import replace_text_variables
 from .base import BaseHandler
 
+from typing import Any
+
 
 class TemporaryVariableHandler(BaseHandler[TemporaryVariable]):
     async def handle(
         self, update: Update, variable: TemporaryVariable, context: HandlerContext
     ) -> list[Connection]:
-        context.variables.store[variable.name] = await replace_text_variables(
+        temporary: dict[str, Any] = context.variables.store.setdefault('TEMPORARY', {})
+        temporary[variable.name] = await replace_text_variables(
             variable.value, context.variables
         )
         return variable.source_connections
